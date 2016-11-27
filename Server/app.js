48184ac3e1c38d4780641aa8002000a78abae8c1
Thomas Van Doren <thomas.vandoren@gmail.com>
2016-11-27 15:06:32 -0800

    Add jwt verification as express middleware.

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,22 @@
 'use strict'
 
 import awsServerlessExpressMiddleware from 'aws-serverless-express/middleware'
+import bodyParser from 'body-parser'
 import cors from 'cors'
 import express from 'express'
-import bodyParser from 'body-parser'
+import jwt from 'express-jwt'
+import {getAuth0Secret} from './helpers/kmsTools'
 
 const app = express()
+
+getAuth0Secret().then((data) => {
+  app.use(jwt({
+    secret: auth0Secret,
+    credentialsRequired: true,
+  }))
+}).catch((err) => {
+  throw new Error(err)
+})
 
 app.use(cors())
 app.use(bodyParser.json())
--- Server/helpers/kmsTools.js
+++ Server/helpers/kmsTools.js
@@ -1,11 +1,12 @@
 import {KMS} from "aws-sdk"
 
 const auth0Secret = {
-    // "KeyId": "arn:aws:kms:us-west-2:298206515678:key/2a9e8b54-4d17-443c-9ef1-2b6ce43f412d",
-    "CiphertextBlob": Buffer("AQECAHhae99zvssV0Z+gn77B+Vi5dLqB1BoNh93meApKFBCITAAAAKIwgZ8GCSqGSIb3DQEHBqCBkTCBjgIBADCBiAYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAxQi/DYu8pEaQ4soskCARCAWxdprWL78kWWkxRG4L7T8I0igxVhN3S/De2rjU9Oek0Zuu6aTVpzryFjCzpu5nVjAtTpXZosQ3UgteFLrMXdKYQ+14tU0K7Osw+RGfoHkeGTb4M5daldYWqPRZk=", 'base64')
+    "CiphertextBlob": Buffer("AQECAHg2JpjKdNCumSgN2xQLw3T5OOS6lVjvxC9t4PJoCGVubgAAAKIwgZ8GCSqGSIb3DQEHBqCBkTCBjgIBADCBiAYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAz8tb6vogF9Dek2lpACARCAW3DTXvwcfOMrBkBT/AJOjJ1sxSSKHKxCMNG5VFfKGTzzKqluxKT3HNBRjhIik7CME/rYGZk6//4za+fs0zX9qSmiCFVO8yWE9vmfY2MPe3qZOqI5QBfZSKMAOY0=", 'base64')
 };
 
-const kms = new KMS();
+const kms = new KMS({
+  region: 'us-west-2'
+});
 
 export function getAuth0Secret() {
     return new Promise((resolve, reject) => {

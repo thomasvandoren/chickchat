8ad12ac2a599019f108f893f1fd7fd2d25d68a9d
Brian Dang <brian@98point6.com>
2016-12-01 22:21:29 -0800

    Document shape of message

--- WebApp/src/reducers/messagesReducer.js
+++ WebApp/src/reducers/messagesReducer.js
@@ -4,8 +4,23 @@ const initialState = []
 
 export default createReducer(initialState, {
     GET_MESSAGES_SUCCESS (state, response) {
-        /** Exercise 1: Update redux state from response **/
+
+        /* Exercise 1: Update redux state from response */
 
         return state
     }
 })
+
+/**
+ * Message shape: {
+ *   author: {
+ *     name: author's full name (string),
+ *     picture: author's profile image source (string),
+ *     userId: author unique ID (string)
+ *   },
+ *   text: message text (string),
+ *   data: message image source (string),
+ *   timestampUtc: message timestamp (number),
+ *   messageId: message unique ID (string)
+ * }
+ */

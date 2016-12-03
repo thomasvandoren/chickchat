a35631645f0c07ea0fb134754aca5cddd0cf62ba
Brian Dang <brian@98point6.com>
2016-12-03 12:28:26 -0800

    Scroll on component update

--- WebApp/src/components/Chat.js
+++ WebApp/src/components/Chat.js
@@ -6,7 +6,7 @@ export class Chat extends React.Component {
     render () {
         return (
             <div style={rootStyle}>
-                <ul style={ulStyle}>
+                <ul style={ulStyle} ref="messages">
                     {this.props.messages.map(renderMessage)}
                 </ul>
 
@@ -14,6 +14,17 @@ export class Chat extends React.Component {
 
             </div>
         )
+    }
+
+    componentDidUpdate (prevProps) {
+        if (prevProps.messages.length === this.props.messages.length) {
+            return
+        }
+
+        const element = this.refs.messages
+        if (element) {
+            element.scrollTop = element.scrollHeight
+        }
     }
 }
 
